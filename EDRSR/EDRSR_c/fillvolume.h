780e5ae40255422ce303664950efd99a00e4c975
Gael Goret <goret@zaltera.esrf.fr>
2012-04-11 14:18:50 +0200

    add to fillvolume.h prototype for func_somme
    Signed-off-by: Gael Goret <goret@esrf.fr>

--- EDRSR/EDRSR_c/fillvolume.h
+++ EDRSR/EDRSR_c/fillvolume.h
@@ -2,3 +2,12 @@
 
 void pippo(int nz, int ny, int nx,     float *volume )  ; 
 
+void func_somme(float  q0x ,float q0y ,float q0z,
+	       float  dqx ,float dqy ,float dqz ,
+	       int nz,int ny,int nx,float *Volume,
+	       float *Mask, 
+	       int dim1,int dim2, 
+	       float *Qfin,
+	       float *data,
+	       float *POL_tmp,
+	       float *C3  );
